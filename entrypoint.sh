dca57c19ecab5390a8cbc4aeb96475d7a46fb840
Mike S <stivaktakis@gmail.com>
2016-03-24 00:08:54 +0200

    fix the pid path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 
 crond
 
-# Remove incompletely-shutdown httpd
-# context after restarting the container.  httpd won't start correctly
-# if it thinks it is already running.
-rm -rf /var/run/apache2/httpd.pid
+# Remove incompletely-shutdown httpd context after restarting the container.  
+# httpd won't start correctly if it thinks it is already running.
+rm -rf /run/apache2/httpd.pid
 rm -rf /var/run/crond.pid
 
-#/usr/sbin/httpd -D FOREGROUND
-
-exec sh -c "usr/sbin/httpd -D FOREGROUND"
+exec sh -c "/usr/sbin/httpd -D FOREGROUND"
